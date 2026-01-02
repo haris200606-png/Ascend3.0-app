eda68e2c09fe7ab4af247cfd6fdff1a4ebae1203
haris200606-png <haris200606@gmail.com>
2026-01-01 22:43:28 -0500

    Create Service-worker.js

---
+++ Service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("ascend").then(cache =>
+      cache.addAll(["./","index.html","styles.css","app.js"])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
